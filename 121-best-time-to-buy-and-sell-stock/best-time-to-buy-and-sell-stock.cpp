7c245acf83875f65d54d22b66c6407a8d262907b
Shrusti Moon <shrustimoon@gmail.com>
2026-06-04 17:50:26 +0530

    Time: 0 ms (100.00%) | Memory: 97.3 MB (59.91%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -5,8 +5,8 @@ public:
         int maxProfit = 0;
         for(int i=0; i<prices.size(); i++){
             int cost = prices[i] - mini;
-            maxProfit = max(cost,maxProfit);
             mini = min(mini,prices[i]);
+            maxProfit = max(cost,maxProfit);
         }
         return maxProfit;
     }
