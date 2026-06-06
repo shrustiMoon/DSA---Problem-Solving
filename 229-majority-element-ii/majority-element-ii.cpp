98cb6d0dd19cfda67366ffabedc65abe7d42baa9
Shrusti Moon <shrustimoon@gmail.com>
2026-06-06 21:30:22 +0530

    Time: 0 ms (100.00%) | Memory: 23.6 MB (62.52%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.cpp
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -1,15 +1,36 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        vector<int>ls;
-        int mini = (nums.size()/3)+1;
-        unordered_map<int,int>mpp;
+        int cnt1 = 0, cnt2 = 0;
+        int el1 = INT_MIN;
+        int el2 = INT_MIN;
+
         for(int i=0; i<nums.size(); i++){
-            mpp[nums[i]]++;
-            if(mpp[nums[i]]==mini){
-            ls.push_back(nums[i]);
+            if(cnt1==0 && el2!=nums[i]){
+                cnt1++;
+                el1 = nums[i];
+            }
+            else if(cnt2==0 && el1!=nums[i]){
+                cnt2++;
+                el2 = nums[i];
+            }
+            else if(nums[i]==el1) cnt1++;
+            else if(nums[i]==el2) cnt2++;
+            else {
+                cnt1-- , cnt2--;
+            }
         }
+        vector<int>ls;
+        cnt1 = 0; cnt2 = 0;
+        for(int i=0; i<nums.size(); i++){
+            if(el1==nums[i]) cnt1++;
+            if(el2==nums[i]) cnt2++;
         }
+        int mini = (nums.size()/3)+1;
+        if(cnt1>=mini) ls.push_back(el1);
+        if(cnt2>=mini) ls.push_back(el2);
+        sort(ls.begin(),ls.end());
         return ls;
+
     }
 };
