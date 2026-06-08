5a6b5e473c41a19e0f5b1b2c7d578c6dc36731f8
Shrusti Moon <shrustimoon@gmail.com>
2026-06-08 21:22:00 +0530

    Time: 0 ms (100.00%) | Memory: 23.8 MB (81.84%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,20 +1,26 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
+        // 2-pointer Approach
+        int j = -1;
         int n = nums.size();
-        vector<int>temp;
-        
-        // Add non-zeros elements to temp
+
+        // Find the first 0 element
         for(int i=0; i<n; i++){
-            if(nums[i]!=0) temp.push_back(nums[i]);
+            if(nums[i]==0){
+                j = i;
+                break;
+            }
         }
-        // Add elements back into nums
-        for(int i=0; i<temp.size(); i++){
-            nums[i] = temp[i];
-        }
-        // Add the 0s to remaining places
-        for(int i=temp.size(); i<n; i++){
-            nums[i] = 0;
+
+        if(j==-1) return;
+
+        // To find non-zero element & swap
+        for(int i=j+1; i<n; i++){
+            if(nums[i]!=0){
+                swap(nums[i],nums[j]);
+                j++;
+            }
         }
     }
 };
