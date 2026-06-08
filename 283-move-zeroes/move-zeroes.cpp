4333de02d1493fad093757eeb699e1e5644c751c
Shrusti Moon <shrustimoon@gmail.com>
2026-06-08 16:35:56 +0530

    Time: 0 ms (100.00%) | Memory: 23.8 MB (54.56%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -2,17 +2,25 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         int n = nums.size();
-        vector<int>temp;
+        int j = -1;
 
+        // Find 1st occurence of 0
         for(int i=0; i<n; i++){
-            if(nums[i]!=0)
-               temp.push_back(nums[i]);
+            if(nums[i]==0){
+                j = i;
+                break;
+            }
         }
-        for(int i=0; i<temp.size();i++){
-            nums[i] = temp[i];
-        }
-        for(int i=temp.size(); i<n; i++){
-            nums[i] = 0;
+
+        // When no 0s exists
+        if(j==-1) return;
+
+        // Find non-zero element & swap them with 0s
+        for(int i=j+1; i<n; i++){
+            if(nums[i]!=0){
+                swap(nums[i],nums[j]);
+                j++;
+            }
         }
     }
 };
