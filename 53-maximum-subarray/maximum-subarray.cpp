9994e86413a4c77f27dbf9864330d46c99941f7b
Shrusti Moon <shrustimoon@gmail.com>
2026-06-10 20:14:20 +0530

    Time: 0 ms (100.00%) | Memory: 71.9 MB (14.51%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -5,10 +5,13 @@ public:
         int maxi = INT_MIN;
 
         for(int i=0; i<nums.size(); i++){
+            // Calculate sum 
             sum = sum + nums[i];
 
+            // If sum greater than maxi, update it
             if(sum>maxi) maxi = sum;
 
+            // If sum becomes negative, Update it to 0
             if(sum<0) sum = 0;
         }
         return maxi;
