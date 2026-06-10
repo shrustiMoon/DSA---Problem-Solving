f4e26e20cfb7c19e9733397155be5650c64e21e5
Shrusti Moon <shrustimoon@gmail.com>
2026-06-10 18:09:08 +0530

    Time: 4 ms (51.46%) | Memory: 14.8 MB (54.67%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -4,11 +4,15 @@ public:
         unordered_map<int,int>mpp;
         for(int i=0; i<nums.size(); i++){
             int a = nums[i];
+            // Calculate the required number
             int req = target - a;
 
+            // Find the required number
             if(mpp.find(req)!=mpp.end()){
                 return {mpp[req],i};
             }
+
+            // Store the number in map
             mpp[a] = i;
         }
         return {};
