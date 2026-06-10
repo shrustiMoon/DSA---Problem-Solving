59882e5a65bb05984a907954fd4c40d8e2e5aa22
Shrusti Moon <shrustimoon@gmail.com>
2026-06-10 16:06:10 +0530

    Time: 1 ms (17.43%) | Memory: 23.9 MB (54.62%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -5,17 +5,16 @@ public:
         int j = -1;
         int n = nums.size();
 
-        // Find the first 0 element
+        // Find the first occurence of 0
         for(int i=0; i<n; i++){
             if(nums[i]==0){
                 j = i;
                 break;
             }
         }
-
         if(j==-1) return;
-
-        // To find non-zero element & swap
+        
+        // Swap the non-zero element with 0
         for(int i=j+1; i<n; i++){
             if(nums[i]!=0){
                 swap(nums[i],nums[j]);
