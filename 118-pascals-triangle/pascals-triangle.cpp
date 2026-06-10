79ad9a710ba537e27f732b1a651fb74570c5f422
Shrusti Moon <shrustimoon@gmail.com>
2026-06-11 00:47:48 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (89.44%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -1,12 +1,12 @@
 class Solution {
-public:
-    vector<int>generateRows(int row){
-        long long ans = 1;
+public: 
+    vector<int>generateRows(int rows){
+        int ans = 1;
         vector<int>ansRow;
         ansRow.push_back(1);
 
-        for(int col=1; col<row; col++){
-            ans = ans * (row-col);
+        for(int col=1; col<rows; col++){
+            ans = ans * (rows-col);
             ans = ans / col;
             ansRow.push_back(ans);
         }
