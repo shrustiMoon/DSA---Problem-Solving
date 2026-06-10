6c53ffc5670a89a2c9e3ffe69496009f032ff31a
Shrusti Moon <shrustimoon@gmail.com>
2026-06-10 20:02:10 +0530

    Time: 0 ms (100.00%) | Memory: 28.1 MB (63.17%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        // MOORES VOTING ALGORITHM
+        // MOORES VOTING ALGORITHMN
         int cnt = 0;
         int element;
 
         for(int i=0; i<nums.size(); i++){
-            // When cnt becomes 0
             if(cnt == 0){
-                cnt = 1;
                 element = nums[i];
+                cnt++;
             }
-            // When same element, increase the cnt
-            else if(element == nums[i]) cnt++;
-            // When different element, decrease the cnt
-            else cnt--;
+            else if(nums[i]==element){
+                cnt++;
+            }
+            else{
+                cnt--;
+            }
         }
         return element;
-        
     }
 };
