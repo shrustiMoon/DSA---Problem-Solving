917f55421749f858202e54207961002c705398b4
Shrusti Moon <shrustimoon@gmail.com>
2026-06-10 22:22:05 +0530

    Time: 0 ms (100.00%) | Memory: 20.8 MB (49.69%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -4,9 +4,10 @@ public:
         int n = matrix.size();
         int m = matrix[0].size();
 
-        vector<int> row(n, 0);
-        vector<int> col(m, 0);
+        vector<int>row(n,0);
+        vector<int>col(m,0);
 
+        // Mark the array index where found 0
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
                 if(matrix[i][j]==0){
@@ -15,11 +16,14 @@ public:
                 }
             }
         }
+        // Convert to to zeros
         for(int i=0; i<n; i++){
             for(int j=0; j<m; j++){
                 if(row[i]==1 || col[j]==1)
                    matrix[i][j] = 0;
             }
         }
+
+
     }
 };
