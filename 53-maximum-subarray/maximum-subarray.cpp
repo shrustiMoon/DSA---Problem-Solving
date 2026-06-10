9b6e8e74b5df5d2a4becffe711d44613965b6381
Shrusti Moon <shrustimoon@gmail.com>
2026-06-10 20:11:54 +0530

    Time: 0 ms (100.00%) | Memory: 71.9 MB (14.51%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        // KADANE's ALGORITHMN
         int sum = 0;
         int maxi = INT_MIN;
 
         for(int i=0; i<nums.size(); i++){
             sum = sum + nums[i];
 
-            // Update the maximun sum
             if(sum>maxi) maxi = sum;
 
-            // Reset the sum
-            if(sum<0) sum =0;
+            if(sum<0) sum = 0;
         }
         return maxi;
     }
