6ffa6bbbf3d066b361794435d899b4b7e5c12ba6
Shrusti Moon <shrustimoon@gmail.com>
2026-06-17 15:08:05 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (54.05%) - LeetSync

---
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string removeOuterParentheses(string s) {
+        string result = "";
+        int cnt = 0;
+
+        for(char ch : s){
+            if(ch == '('){
+                if(cnt > 0) result += ch;
+                cnt++;
+            }
+            else {
+                if(ch == ')'){
+                    cnt--;
+                    if(cnt > 0) result += ch;
+                }
+            }
+        }
+        return result;
+    }
+};
