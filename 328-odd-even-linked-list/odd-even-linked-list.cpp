1c8804ae7e2526a72b43f17efd9d021a424c588d
Shrusti Moon <shrustimoon@gmail.com>
2026-06-28 12:27:36 +0530

    Time: 0 ms (100.00%) | Memory: 15.7 MB (7.96%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.cpp
+++ 328-odd-even-linked-list/odd-even-linked-list.cpp
@@ -21,10 +21,10 @@ public:
         while(even!=NULL && even->next!=NULL){
             odd->next = odd->next->next;
             even->next = even->next->next;
-
+            
             odd = odd->next;
             even = even->next;
-        }
+        } 
         odd->next = evenHead;
         return head;
     }
