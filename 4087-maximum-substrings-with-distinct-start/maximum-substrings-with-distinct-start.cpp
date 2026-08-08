409d0a731442b08d05760cbf35e10389dc044b84
Shrusti Moon <shrustimoon@gmail.com>
2026-08-08 10:12:37 +0530

    Time: 28 ms (51.68%) | Memory: 25.3 MB (25.79%) - LeetSync

--- 4087-maximum-substrings-with-distinct-start/maximum-substrings-with-distinct-start.cpp
+++ 4087-maximum-substrings-with-distinct-start/maximum-substrings-with-distinct-start.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int maxDistinct(string s) {
-        set<char>st;
+        unordered_set<char>st;
         for(int i=0; i<s.size(); i++){
             st.insert(s[i]);
         }
