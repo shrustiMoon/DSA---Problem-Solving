44e1e37f7ca813bcfe752bf598157328a2711a9b
Shrusti Moon <shrustimoon@gmail.com>
2026-09-23 00:38:51 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (7.59%) - LeetSync

--- 169-majority-element/majority-element.cpp
+++ 169-majority-element/majority-element.cpp
@@ -5,17 +5,16 @@ public:
         int cnt = 0;
         int element;
 
+        // If cnt becomes 0
         for(int i=0; i<nums.size(); i++){
             if(cnt == 0){
+                cnt = cnt + 1;
                 element = nums[i];
-                cnt++;
             }
-            else if(nums[i]==element){
-                cnt++;
-            }
-            else{
-                cnt--;
-            }
+            // If same element found, increase cnt
+            else if(nums[i]==element) cnt++;
+            // If different element found, decrease cnt
+            else cnt--;
         }
         return element;
     }
