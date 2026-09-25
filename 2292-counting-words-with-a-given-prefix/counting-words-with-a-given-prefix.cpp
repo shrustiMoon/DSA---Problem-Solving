3870edc361984b98179664c24ef3eba9b0a4b6ce
Shrusti Moon <shrustimoon@gmail.com>
2026-09-25 11:55:32 +0530

    Time: 0 ms (100.00%) | Memory: 13.1 MB (83.17%) - LeetSync

---
+++ 2292-counting-words-with-a-given-prefix/counting-words-with-a-given-prefix.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int prefixCount(vector<string>& words, string pref) {
+        int cnt = 0;
+
+        for(int i=0; i<words.size(); i++){
+            if(words[i].find(pref) == 0) 
+               cnt++;
+        }
+        return cnt;
+    }
+};
