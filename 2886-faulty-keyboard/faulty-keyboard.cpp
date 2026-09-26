9d029c2d829282b1557ea3cba57f0b8c6eca3246
Shrusti Moon <shrustimoon@gmail.com>
2026-09-26 14:28:47 +0530

    Time: 3 ms (40.92%) | Memory: 10 MB (11.49%) - LeetSync

---
+++ 2886-faulty-keyboard/faulty-keyboard.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string finalString(string s) {
+        string ans = "";
+        for(int i=0; i<s.size(); i++){
+            if(s[i] != 'i'){
+                ans = ans + s[i];
+            }
+            else{
+                reverse(ans.begin(), ans.end());
+            }
+        }
+        return ans;
+    }
+};
